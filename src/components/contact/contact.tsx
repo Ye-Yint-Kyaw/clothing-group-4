e3934025b7d56d441491d54e901652022e2ab6bd
Macbook Air <macbookair@Macbooks-MacBook-Air.local>
2023-03-17 15:11:18 +0630

    Input Component Updated

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,5 +1,6 @@
 import Formbtn from "./formbtn";
 import styles  from "./contact.module.css";
+import Input from "./contactinput";
 const Contact= ()=>{
   return(
   
@@ -9,17 +10,14 @@ const Contact= ()=>{
                         <div className={styles.formcardBlog}>
                           <h1 className={styles.title}>Reach Out To Us</h1>
                           <div className={styles.inputBox}>
-                            <div className={styles.inputText}>
-                              <input type="text" placeholder="FirstName" />
-                              <input type="text" placeholder="LastName" />
+                            <div className={styles.inputWrap}>
+                              <Input type="text" placeholder="First name..."/>
+                              <Input type="text" placeholder="LastName..." />
                             </div>
-                            <div className={styles.inputMail}>
-                              <input type="email" placeholder="Email" />
-                              <input type="password" placeholder="Password" />
+                            <div className={styles.inputWrap}>
+                              <Input type="email" placeholder="Email..."/>
+                              <Input type="tel" placeholder="Phone Number..." />
                             </div>
-                          </div>
-                          <div className={styles.addressInput}>
-                            <input type="address" placeholder="Address" />
                           </div>
                           <div className={styles.feedbackBox}>
                             <textarea
